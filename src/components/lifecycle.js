b9d31b2a7683cc1531a741134bd40d46ec8e6977
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-03-04 13:44:02 +0500

    lifecycle|Bind finished

--- src/components/lifecycle.js
+++ src/components/lifecycle.js
@@ -4,27 +4,65 @@ import React from 'react';
 class Header extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {favoritecolor: "red"};
+    this.state = { count: 1 };
+    // this.onplus = this.onplus.bind(this)
   }
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({favoritecolor: "yellow"})
-    }, 1000)
-  }
-  shouldComponentUpdate() {
-    return true;
-  }
-  changeColor = () => {
-    this.setState({favoritecolor: "blue"});
-  }
+   onplus(){
+    this.setState({count:this.state.count + 1})
+    console.log(this);
+ }
   render() {
+    // const onplus = () => {
+    //    this.setState({count:this.state.count + 1})
+    // }
     return (
       <div>
-        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
-      <button type="button" onClick={this.changeColor}>Change color</button>
-      </div>
-    );
+        <h1>Count:{this.state.count }</h1>
+        {/* <button onClick={this.onplus.bind(this)}>+</button> */}
+        <button onClick={()=>this.onplus()}>+</button>
 
+      </div>
+    )
   }
 }
 export default Header
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Header extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {favoritecolor: "red"};
+//   }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({favoritecolor: "yellow"})
+//     }, 2000)
+//   }
+//   shouldComponentUpdate() {
+//     return false;
+//   }
+//   changeColor = () => {
+//     this.setState({favoritecolor: "blue"});
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+//       <button type="button" onClick={this.changeColor}>Change color</button>
+//       </div>
+//     );
+
+//   }
+// }
+// export default Header
