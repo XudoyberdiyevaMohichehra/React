50302cd55438fa4979439399cd2238a63e497f46
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-03-04 11:58:35 +0500

    lifecycle

---
+++ src/components/lifecycle.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({favoritecolor: "yellow"})
+    }, 1000)
+  }
+  shouldComponentUpdate() {
+    return true;
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+        <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+
+  }
+}
+export default Header
