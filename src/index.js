6f659cf7379e0b8b3fc5e9a7561ef3ac1c931e5b
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-03-08 21:29:15 +0500

    localStorage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Header from './components/lifecycle';
+import Header from './localstorage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/localstorage.jsx
@@ -0,0 +1,30 @@
+
+// import { useState } from "react";
+
+// const Hooks =()=>{
+//   const [count, setCount] = useState(Number(localStorage.getItem('count')))
+  
+  
+//   const plus=()=>{
+//     localStorage.setItem('count', count + 1)
+//     setCount(count + 1)
+//   }
+//   const minus=()=>{
+//     localStorage.setItem('count', count - 1)
+//     setCount(count - 1)
+//   }
+//     return(
+//        <div style={{ flex: 1 }}>
+//         <h1>Hooks Component</h1>
+//         <h1>Count:{count}</h1>
+//         <button onClick={plus}>+</button>
+//         <button onClick={minus}>-</button>
+//        </div>
+//     )
+// }
+
+// export default Hooks;
+
+
+
+
